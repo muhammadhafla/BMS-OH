0ff43c24f97cf9baa97033126c1612b229259ef6
muhammad hafla <muhammadhafla@gmail.com>
2025-09-29 03:29:18 +0000

    Try fixing this error: `Runtime Error: Error: ./src/app/(app)/layout.tsx

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { AppSidebar, AppSidebarTrigger } from '@/components/shared/app-sidebar';
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
-import { cookies } from 'next/headers';
 import { usePathname } from 'next/navigation';
 
 export default function AppLayout({
@@ -10,8 +9,6 @@ export default function AppLayout({
   children: React.ReactNode;
 }>) {
   const pathname = usePathname();
-  // const layout = cookies().get('sidebar_state');
-  // const defaultOpen = layout ? layout.value === 'true' : true;
   const defaultOpen = true;
 
   // Render POS page without the sidebar layout
